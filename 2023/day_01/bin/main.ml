c0241d02ee03f9e66f8ecb106125b8fd2ef522d5
jdavasligil <22987230+jdavasligil@users.noreply.github.com>
2023-12-02 12:42:30 -0800

    Solve day 1 part 2

--- 2023/day_01/bin/main.ml
+++ 2023/day_01/bin/main.ml
@@ -8,34 +8,127 @@ let is_digit c =
     '0' .. '9' -> true
     | _ -> false;;
 
+let is_digit_prefix pre =
+    match pre with
+    | "o" -> true
+    | "t" -> true
+    | "f" -> true
+    | "s" -> true
+    | "e" -> true
+    | "n" -> true
+    | _ -> false
+
+let next_prefix pre =
+    match pre with
+    | "on" -> "one"
+    | "tw" -> "two"
+    | "th" -> "thr"
+    | "thr" -> "thre"
+    | "thre" -> "three"
+    | "fo" -> "fou"
+    | "fou" -> "four"
+    | "fi" -> "fiv"
+    | "fiv" -> "five"
+    | "si" -> "six"
+    | "se" -> "sev"
+    | "sev" -> "seve"
+    | "seve" -> "seven"
+    | "ei" -> "eig"
+    | "eig" -> "eigh"
+    | "eigh" -> "eight"
+    | "ni" -> "nin"
+    | "nin" -> "nine"
+    | _ -> ""
+    
+let is_digit_str s = 
+    match s with
+    | "one" -> true
+    | "two" -> true
+    | "three" -> true
+    | "four" -> true
+    | "five" -> true
+    | "six" -> true
+    | "seven" -> true
+    | "eight" -> true
+    | "nine" -> true
+    | _ -> false
+
+let digit_str_to_int s = 
+    match s with
+    | "one" -> 1
+    | "two" -> 2
+    | "three" -> 3
+    | "four" -> 4
+    | "five" -> 5
+    | "six" -> 6
+    | "seven" -> 7
+    | "eight" -> 8
+    | "nine" -> 9
+    | _ -> 0
+
+let rec try_digit_from_str s head tail len =
+
+    if tail < len then 
+
+        let substr = String.sub s head (tail - head) in
+        let next_substr = String.sub s head (tail + 1 - head) in
+
+        if is_digit_str substr then
+            digit_str_to_int substr
+        else if (next_prefix substr) = next_substr then
+            try_digit_from_str s head (tail + 1) len
+        else
+            -tail + 1
+
+    else if tail = len then
+
+        let substr = String.sub s head (tail - head) in
+
+        if is_digit_str substr then
+            digit_str_to_int substr
+        else
+            -tail + 1
+    else
+        -tail + 1
+
 let rec first_digit s idx len =
+
     if idx < len then
         if is_digit (String.get s idx) then
             Some (Char.code (String.get s idx) - (Char.code '0'))
+
+        else if is_digit_prefix (String.sub s idx 1) then
+            let maybe_digit = try_digit_from_str s idx (idx + 2) len in
+            if maybe_digit > 0 then
+                Some maybe_digit
+            else
+                first_digit s (-maybe_digit) len
+
         else 
             first_digit s (idx + 1) len
     else
         None
 
-let rec last_digit s idx =
-    if idx >= 0 then
-        if is_digit (String.get s idx) then
-            Some (Char.code (String.get s idx) - (Char.code '0'))
-        else 
-            last_digit s (idx - 1)
-    else
-        None
+let rec last_digit s idx len =
+    match (first_digit s idx len) with
+    | Some x -> x
+    | None -> if idx > 0 then last_digit s (idx - 1) len else -1
+
+(*
+let rev_str s = String.init (String.length s) ((fun s i -> String.get s (String.length s - i - 1)) s) 
+*)
 
 let two_digit s = 
     let first = match (first_digit s 0 (String.length s)) with
     | Some x -> x
     | None -> raise (NoDigit "First digit not found!") in
 
-    let last = match (last_digit s (String.length s - 1)) with
-    | Some x -> x
-    | None -> raise (NoDigit "Last digit not found!") in
+    let last = last_digit s (String.length s - 1) (String.length s) in
 
-    (first * 10) + last
+    if (last = -1) then
+        raise (NoDigit "Last digit not found!")
+    else
+        (first * 10) + last
 
 let rec sum_digits ic total =
     try
