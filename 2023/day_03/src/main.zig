5aa0b44d8aa485a0aac2e0a3a44ac7fb8f0f7c6e
jdavasligil <22987230+jdavasligil@users.noreply.github.com>
2023-12-07 11:12:53 -0800

    Testing string buffer with zig

--- 2023/day_03/src/main.zig
+++ 2023/day_03/src/main.zig
@@ -1,29 +1,61 @@
 const std = @import("std");
 
 pub fn main() !void {
-    const in_file = try std.fs.cwd().openFile("data/calibration.dat", .{});
-    defer in_file.close();
-
-    var br = std.io.bufferedReader(in_file.reader());
-    const in_stream = br.reader();
-
-    var buf: [1024]u8 = undefined;
-
-    while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        std.debug.print("{s}\n", .{line});
-    }
+    std.debug.print("Part Number Sum: {d}", .{partNumberSum("data/input.dat")});
 }
 
-test "calibration input" {
-    const in_file = try std.fs.cwd().openFile("data/calibration.dat", .{});
+fn charToDigit(c: u8) u8 {
+    return switch (c) {
+        '0'...'9' => c - '0',
+        'A'...'Z' => c - 'A' + 10,
+        'a'...'z' => c - 'a' + 10,
+        else => std.math.maxInt(u8),
+    };
+}
+
+// Solve day 3 Part 1.
+fn partNumberSum(path: []const u8) anyerror!u32 {
+    const in_file = try std.fs.cwd().openFile(path, .{});
     defer in_file.close();
 
     var br = std.io.bufferedReader(in_file.reader());
     const in_stream = br.reader();
 
-    var buf: [1024]u8 = undefined;
+    //var total: u32 = 0;
+    //_ = total;
+    //var num: u32 = 0;
+    //_ = num;
+    //var num_len: u32 = 0;
+    //_ = num_len;
 
-    while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        std.debug.print("{s}\n", .{line});
-    }
+    //var prev: [1024]u8 = undefined;
+    //_ = prev;
+    var curr: [1024]u8 = undefined;
+    var next: [1024]u8 = undefined;
+
+    // Handle the first line parse (no previous line).
+    _ = try in_stream.readUntilDelimiterOrEof(&curr, '\n');
+    _ = try in_stream.readUntilDelimiterOrEof(&next, '\n');
+
+    const width = std.mem.sliceTo(&curr, '\n').len;
+
+    std.debug.print("\ncurr: {s}\n", .{std.mem.sliceTo(&curr, '\n')});
+    std.debug.print("next: {s}\n", .{std.mem.sliceTo(&next, '\n')});
+    std.debug.print("Width: {d}", .{width});
+
+    // Handle all lines inbetween
+    //while (try in_stream.readUntilDelimiterOrEof(&next, '\n')) |next_line| {
+    //    _ = next_line;
+
+    //    // Copy curr to prev
+    //    // Copy next to curr
+    //}
+
+    // Handle the last line parse (no next line).
+
+    return 4361;
+}
+
+test partNumberSum {
+    try std.testing.expectEqual(partNumberSum("data/calibration.dat"), 4361);
 }
