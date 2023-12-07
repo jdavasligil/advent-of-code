3588c972b9d9b21360388405dbaed6b73fd87aea
jdavasligil <22987230+jdavasligil@users.noreply.github.com>
2023-12-07 13:01:23 -0800

    Get parser to work for first line

--- 2023/day_03/src/main.zig
+++ 2023/day_03/src/main.zig
@@ -4,6 +4,32 @@ pub fn main() !void {
     std.debug.print("Part Number Sum: {d}", .{partNumberSum("data/input.dat")});
 }
 
+fn isSymbol(c: u8) bool {
+    return switch (c) {
+        '0'...'9' => false,
+        'A'...'Z' => false,
+        'a'...'z' => false,
+        '.' => false,
+        else => true,
+    };
+}
+
+fn powOfTen(p: usize) u32 {
+    return switch (p) {
+        0 => 1,
+        1 => 1E1,
+        2 => 1E2,
+        3 => 1E3,
+        4 => 1E4,
+        5 => 1E5,
+        6 => 1E6,
+        7 => 1E7,
+        8 => 1E8,
+        9 => 1E9,
+        else => std.math.maxInt(u32),
+    };
+}
+
 fn charToDigit(c: u8) u8 {
     return switch (c) {
         '0'...'9' => c - '0',
@@ -21,12 +47,13 @@ fn partNumberSum(path: []const u8) anyerror!u32 {
     var br = std.io.bufferedReader(in_file.reader());
     const in_stream = br.reader();
 
-    //var total: u32 = 0;
-    //_ = total;
-    //var num: u32 = 0;
-    //_ = num;
-    //var num_len: u32 = 0;
-    //_ = num_len;
+    var total: u32 = 0;
+    var num: u32 = 0;
+    var num_len: u32 = 0;
+    var dist: u32 = 0;
+    var i: usize = 0;
+    var j: usize = 0;
+    var d: u8 = 0;
 
     //var prev: [1024]u8 = undefined;
     //_ = prev;
@@ -37,13 +64,57 @@ fn partNumberSum(path: []const u8) anyerror!u32 {
     _ = try in_stream.readUntilDelimiterOrEof(&curr, '\n');
     _ = try in_stream.readUntilDelimiterOrEof(&next, '\n');
 
-    const width = std.mem.sliceTo(&curr, '\n').len;
+    const width: usize = std.mem.sliceTo(&curr, '\n').len;
 
-    std.debug.print("\ncurr: {s}\n", .{std.mem.sliceTo(&curr, '\n')});
-    std.debug.print("next: {s}\n", .{std.mem.sliceTo(&next, '\n')});
-    std.debug.print("Width: {d}", .{width});
+    std.debug.print(" \n", .{});
+    std.debug.print("Curr: {s}\n", .{std.mem.sliceTo(&curr, '\n')});
+    std.debug.print("Next: {s}\n", .{std.mem.sliceTo(&next, '\n')});
+    std.debug.print("Width: {d}\n", .{width});
+
+    i = width - 1;
 
     // Handle all lines inbetween
+    while (i > 0) {
+        num = 0;
+        num_len = 0;
+
+        // Find first digit from the right
+        while (i > 0 and charToDigit(curr[i]) == std.math.maxInt(u8)) {
+            i -= 1;
+        }
+
+        // Compute number and digit length
+        while (true) {
+            d = charToDigit(curr[i]);
+            if (d == std.math.maxInt(u8)) break;
+            num += d * powOfTen(num_len);
+            num_len += 1;
+
+            if (i > 0) {
+                i -= 1;
+            } else {
+                break;
+            }
+        }
+
+        if (num == 0 and i == 0) break;
+
+        // Determine if number is adjacent to a symbol
+        dist = num_len + 2;
+        j = i;
+        while (dist > 0 and j < width) : ({
+            j += 1;
+            dist -= 1;
+        }) {
+            if (isSymbol(next[j])) {
+                std.debug.print("{c}\n", .{next[j]});
+                total += num;
+                break;
+            }
+        }
+    }
+    std.debug.print("Total: {d}", .{total});
+
     //while (try in_stream.readUntilDelimiterOrEof(&next, '\n')) |next_line| {
     //    _ = next_line;
 
@@ -54,6 +125,13 @@ fn partNumberSum(path: []const u8) anyerror!u32 {
     // Handle the last line parse (no next line).
 
     return 4361;
+}
+
+test powOfTen {
+    try std.testing.expectEqual(powOfTen(0), 1);
+    try std.testing.expectEqual(powOfTen(1), 10);
+    try std.testing.expectEqual(powOfTen(2), 100);
+    try std.testing.expectEqual(powOfTen(3), 1000);
 }
 
 test partNumberSum {
